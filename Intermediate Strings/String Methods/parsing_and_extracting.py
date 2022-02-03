2c5c10f7636d339ebc6292bc452d6e2aa8b00c92
Urmish Ramani <pate0586@algonquinlive.com>
2022-02-03 01:47:27 -0500

    parsing and extracting

---
+++ Intermediate Strings/String Methods/parsing_and_extracting.py
@@ -0,0 +1,13 @@
+"""
+Parsing and Extracting
+- finding host name from email data
+"""
+data = "pinbot@info.pinterest.com Sat, Jan 22, 4:35 AM (12 days ago)"
+at_position = data.find("@")
+print(at_position)
+
+space_position = data.find(" ", at_position)
+print(space_position)
+
+hostname = data[at_position+1: space_position]
+print(hostname)
